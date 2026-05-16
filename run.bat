f24a6c588d22ab98040d081d77a8a30e6bc77acf
Jahnavi <jahnavinelluri01@gmail.com>
2026-05-16 20:39:32 +0530

    changed run.bat file

---
+++ run.bat
@@ -0,0 +1,3 @@
+python -m pytest -v -s -m sanity --html=Reportsreport.html python_selenium_pytesttestcasestest_ultimateqaUI.py                                             
+rem python -m pytest -v -s -m sanity or regression --html=Reportsreport.html python_selenium_pytesttestcases
+rem python -m pytest -v -s -m sanity and regression --html=Reportsreport.html python_selenium_pytesttestcases
